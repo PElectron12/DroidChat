2cb76bb21a1ec94ffbcff5b97ac43e2f14a0bcfc
root <root@localhost.localdomain>
2017-05-08 18:00:03 +0000

    version 0.1 Paras, implemented 'Who are you'

--- version_0/hello.py
+++ version_0/hello.py
@@ -5,5 +5,9 @@ text = text.strip()
 
 if text  in ["hi", "hello", "hey", "ola", "hallo", "wassup", "yo", "hie"]:
     print "Hey, I am Paras ... How are You?"
+elif text in ["who are you?", "who are you", "what are you?", "what are you", "what is your name?", "what is your name",
+ "what do you do?", "what do you do", "what should i call you", "what should i call you?", "tell me your name?", "tell me your named"]:
+	print "I am DroidChat, a ChatBot."
 else:
-    print "I am not in a mood to talk right now!"
+	print "I am not in a mood to talk right now!"
+ 
