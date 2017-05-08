d8e1bd4944655761478cf065f94010dd5e568dc0
shanu khera <shanu.khera@theredpandas.com>
2017-05-08 18:17:09 +0530

    hi and who are you combined

--- version_0/hello.py
+++ version_0/hello.py
@@ -1,13 +1,23 @@
-text = raw_input("Please Enter Your Message:")
+sentence = raw_input("Please Enter Your Message:")
 
-text = text.lower()
-text = text.strip()
+sentence = sentence.lower().strip()
+answered = False
 
-if text  in ["hi", "hello", "hey", "ola", "hallo", "wassup", "yo", "hie"]:
-    print "Hey, I am Paras ... How are You?"
-elif text in ["who are you?", "who are you", "what are you?", "what are you", "what is your name?", "what is your name",
- "what do you do?", "what do you do", "what should i call you", "what should i call you?", "tell me your name?", "tell me your named"]:
-	print "I am DroidChat, a ChatBot."
-else:
+first_word = sentence.split(" ")[0]
+if first_word[-1] in [",", "!", "."]:
+    first_word = first_word[0:-1]
+
+if first_word  in ["hi", "hello", "hey", "ola", "hallo", "wassup", "yo", "hie", "hii"]:
+    print "Hey There!"
+    answered = True
+
+intro_questions = ["who are you","what are you", "what is your name","what do you do", "what should i call you", "tell me your name"]
+
+for question in intro_questions:
+    if question in sentence:
+        print "Hey, I am Paras ... How are You?"
+        answered = True
+
+if not answered:
 	print "I am not in a mood to talk right now!"
- 
+
